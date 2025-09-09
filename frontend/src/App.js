cc1f69514b56476be450abe067484293e8c646c2
aliestare <alina.nikitina4797@gmail.com>
2025-09-09 19:40:19 +0200

    initial frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+//function App() { another function syntax
+const App = () => {
+  return (
+    //<div className="App">
+    <div>
+      <h1>Images Gallery</h1>
+    </div>
+  );
+}
+export default App;

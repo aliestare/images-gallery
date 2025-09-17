103df421fb570a1d27905efe3be0a8fa7fe4a330
aliestare <alina.nikitina4797@gmail.com>
2025-09-17 12:32:26 +0200

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-
-//function App() { another function syntax
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
+
+
+//function App() { another function syntax
 const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
   return (
     //<div className="App">
     <div>
       <Header title="Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
